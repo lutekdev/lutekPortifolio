231a050755e1762a2b40035aa121c4811fdd11ed
LutekDev <lutekdev@gmail.com>
2023-09-24 00:42:34 -0300

    :tada: Added New Files in GitIgnore

--- lutekBrains/lutekTrinks.js
+++
@@ -1,9 +0,0 @@
-/* --> Body Completo (Modelo Completo de imagem de Fundo) <-- */
-// body {
-// background-image: url(./assets/bg-desktop.jpg);
-// background-repeat: no-repeat;
-// background-size: cover;
-// }
-
-/* --> Criando Bordas <-- */
-// border: 1px solid red;
